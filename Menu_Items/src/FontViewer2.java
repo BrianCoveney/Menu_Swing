9e4d84d4b35dbe6adde321965b7e480a480c626c
Brian Coveney <coveneybrian@gmail.com>
2014-08-02 19:47:49 +0100

    Initial check in of Menu Items

---
+++ Menu_Items/src/FontViewer2.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+/**
+ * This program uses a menu to display font effects
+ */
+
+public class FontViewer2 {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new FontViewer2Frame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+	}
+
+}
